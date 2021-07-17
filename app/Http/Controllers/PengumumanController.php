1b0dbe4b999fd09a6960b137d3b3116dad30318b
dimas diandra <dimasdiandra9@gmail.com>
2021-07-17 17:19:47 +0700

    admin pengumuman, tinggal manggil dibagian user penggumuman

---
+++ app/Http/Controllers/PengumumanController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Pengumuman;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class PengumumanController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('admin.tambahpengumuman');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'judul'=> 'required',
+            'isi_pengumuman' => 'required'
+        ]);
+        $pengumuman= new Pengumuman;
+        $pengumuman->judul = $request->input('judul');
+        $pengumuman->isi_pengumuman = $request->input('isi_pengumuman');
+        $pengumuman->save();
+
+        return redirect('admin/pengumuman')->with('success','Pengumuman Ditambahkan');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show()
+    {
+        $pengumuman = DB::table('pengumumans')->get();
+		return view ('admin.tambahpengumuman', compact('pengumuman'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UploadController;
 use App\Http\Controllers\ReportController;
+use App\Http\Controllers\PengumumanController;
 use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
@@ -42,44 +43,21 @@ Route::get('view', [App\Http\Controllers\FileController::class, 'getFile']);
 Route::get('get/{filename}', [FileController::class, 'getfile']);
 
 
-Route::middleware('role:admin')->get('/admin', function () {
+Route::middleware('role:admin')->get('/admin', function(){
     return view('admin.home');
 })->name('admin');
 Route::middleware('role:admin')->prefix('admin')->group(function () {
-    Route::get('/admin', function () {
-        return view('admin.home');
-    })->name('admin');
+    Route::get('/admin', function () {return view('admin.home');})->name('admin');
     Route::get('/user', [App\Http\Controllers\UserController::class, 'index']);
-    Route::get('/perencanaan', function () {
-        return view('admin.perencanaan');
-    });
-    Route::get('/evaluasi', function () {
-        return view('admin.evaluasi');
-    });
-    Route::get('/template', function () {
-        return view('admin.datatemplate');
-    });
-    Route::get('/pengumuman', function () {
-        return view('admin.tambahpengumuman');
-    });
-});
-
-Route::get('/admin/user', function () {
-    return view('admin.datauser');
-});
-Route::get('/admin/perencanaan', function () {
-    return view('admin.perencanaan');
-});
-Route::get('/admin/evaluasi', function () {
-    return view('admin.evaluasi');
-});
-Route::get('/admin/template', function () {
-    return view('admin.datatemplate');
-});
-Route::get('/admin/pengumuman', function () {
-    return view('admin.tambahpengumuman');
+    Route::get('/perencanaan', function () {return view('admin.perencanaan');});
+    Route::get('/evaluasi', function () {return view('admin.evaluasi');});
+    Route::get('/template', function () {return view('admin.datatemplate');});
+    // Route::get('/pengumuman', function () {return view('admin.tambahpengumuman');});
 });
 
 Route::get('/sidebar', function () {
     return view('sidebar');
 });
+
+Route::get('/admin/pengumuman', [PengumumanController::class, 'show']);
+Route::post('/admin/pengumuman', [PengumumanController::class, 'store']);
