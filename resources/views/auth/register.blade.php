18572ca7601f87ba57ee180aa9c1acf5f5870e10
ediashta <ediashtarevin77@gmail.com>
2021-07-17 19:27:34 +0700

    rever register yang sudah bisa

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,21 +1,27 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center mt-2">
-        <div class="col-md-10">
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="hero text-center">
-                        <img src="{{ asset('assets/register.png') }}" class="img-fluid">
+<head>
+    <meta charset="utf-8">
+    <meta name="author" content="Kodinger">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>My Login Page &mdash; Bootstrap 4 Login Page Snippet</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body class="my-login-page">
+    <section class="h-100">
+        <div class="container h-100">
+            <div class="row justify-content-md-center h-100">
+                <div class="card-wrapper">
+                    <div class="brand">
+                        <img src="assets/pkl.png" alt="bootstrap 4 login page">
                     </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="card">
-                        <div class="card-header">{{ __('Register') }}</div>
-
+                    <div class="card fat">
                         <div class="card-body">
-                            <form method="POST" action="{{ route('register') }}">
+                            <h4 class="card-title">Register</h4>
+                            <form method="POST" class="my-login-validation" novalidate="" action="{{ route('register') }}">
                                 @csrf
                                 <div class="form-group row">
                                     <label for="name" class="col-md-12 col-form-label">{{ __('Name') }}</label>
@@ -67,27 +73,38 @@
                                     </div>
                                 </div>
 
-                                <div class="form-group row">
-                                    <label for="password-confirm" class="col-md-12 col-form-label">{{ __('Confirm Password') }}</label>
-
-                                    <div class="col-md-12">
-                                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                                <div class="form-group">
+                                    <div class="custom-checkbox custom-control">
+                                        <input type="checkbox" name="agree" id="agree" class="custom-control-input" required="">
+                                        <label for="agree" class="custom-control-label">I agree to the <a href="#">Terms and Conditions</a></label>
+                                        <div class="invalid-feedback">
+                                            You must agree with our Terms and Conditions
+                                        </div>
                                     </div>
                                 </div>
 
-                                <div class="form-group row mb-0">
-                                    <div class="col-md-6 offset-md-4">
-                                        <button type="submit" class="btn btn-primary">
-                                            {{ __('Register') }}
-                                        </button>
-                                    </div>
+                                <div class="form-group m-0">
+                                    <button type="submit" class="btn btn-primary btn-block">
+                                        Register
+                                    </button>
+                                </div>
+                                <div class="mt-4 text-center">
+                                    Already have an account? <a href="/">Login</a>
                                 </div>
                             </form>
                         </div>
                     </div>
+                    <div class="footer">
+                        Copyright &copy; 2017 &mdash; Your Company
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
-@endsection
+    </section>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="js/my-login.js"></script>
+</body>
+
+</html>
